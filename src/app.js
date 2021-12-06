b0e533d1cdfacb2da9dc709b938da89ee87df1ce
赖铭志 <ramen.curator@gmail.com>
2021-12-06 19:41:07 +0800

    init

--- src/app.js
+++ src/app.js
@@ -4,27 +4,7 @@ import "./app.css";
 export const TodoContext = React.createContext({});
 
 const App = ({ children }) => {
-  const [items, setItems] = React.useState([
-    { id: 1, text: "Learning 支付宝小程序", completed: true },
-    { id: 2, text: "Learning React", completed: true },
-    { id: 3, text: "Learning Remax", completed: false },
-  ]);
-
-  const test = "hello world is me !";
-
-  return (
-    <TodoContext.Provider
-      value={
-        ({
-          items,
-          setItems,
-          test
-        })
-      }
-    >
-      {children}
-    </TodoContext.Provider>
-  );
+  return children;
 };
 
 export default App;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,12 +2,11 @@ import React, { useState, useRef } from "react";
 import {
   View
 } from "remax/wechat";
-//import { TodoContext } from "@/app";
 import "./index.css";
 
 export default () => {
   return (
-    <View className="container">
+    <View>
     </View>
   );
 };
