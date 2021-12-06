85bbee9fc41ab7de71e3d6bf1eee97ce6b6f15a9
赖铭志 <ramen.curator@gmail.com>
2021-12-06 17:01:39 +0800

    初始化我的实验环境

--- src/app.config.js
+++ src/app.config.js
@@ -1,41 +1,8 @@
 module.exports = {
-  pages: ['pages/index/index','pages/jewelry/jewelry','pages/message/message','pages/my/my'],
+  pages: ['pages/index/index'],
   window: {
-    navigationBarTitleText: '仿闲鱼',
-    navigationBarBackgroundColor: '#ffe60f',
-    navigationBarTextStyle: 'black'
+    navigationBarTitleText: '我的实验环境',
+    navigationBarBackgroundColor: '#282c34',
+    navigationBarTextStyle: 'white'
   },
-  tabBar: {
-    selectedColor: '#ffe60f',
-    list: [
-      {
-        pagePath: 'pages/index/index',
-        iconPath: '/images/home.png',
-        selectedIconPath: '/images/home-select.png',
-        selectedColor: '#ffe60f',
-        text: '首页',
-      },
-      {
-        pagePath: 'pages/jewelry/jewelry',
-        iconPath: '/images/jewelry.png',
-        selectedIconPath: '/images/jewelry-select.png',
-        selectedColor: '#ffe60f',
-        text: '会玩',
-      },
-      {
-        pagePath: 'pages/message/message',
-        iconPath: '/images/message.png',
-        selectedIconPath: '/images/message-select.png',
-        selectedColor: '#ffe60f',
-        text: '信息',
-      },
-      {
-        pagePath: 'pages/my/my',
-        iconPath: '/images/user-center.png',
-        selectedIconPath: '/images/user-center-select.png',
-        selectedColor: '#ffe60f',
-        text: '我的',
-      },
-    ],
-  }
 };
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,29 +1,13 @@
 import React, { useState, useRef } from "react";
 import {
-  View,
-  Input,
-  Image,
-  Label,
-  Text,
-  navigateTo,
+  View
 } from "remax/wechat";
-import HomeInput from "./home-input";
-import FirstCard from "./first-card";
-import SecondCard from "./second-card";
-import ThridCard from "./thrid-card";
 //import { TodoContext } from "@/app";
 import "./index.css";
 
 export default () => {
   return (
     <View className="container">
-      <HomeInput />
-      {/* 第一个方块 */}
-      <FirstCard />
-      {/* 第二个方块 */}
-      <SecondCard />
-      {/* 第三个部分 */}
-      <ThridCard />
     </View>
   );
 };
